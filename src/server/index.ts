ed4b73ad50cac54f267e1c13b26ba04be033e63f
jack-dark <decker3d@gmail.com>
2026-02-23 11:55:12 -0800

    feat: commits lesson updates

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,22 @@
-import amqp from "amqplib";
+import amqp, { type ConfirmChannel } from "amqplib";
+import { publishJSON } from "../internal/gamelogic/pubsub/index.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(rabbitConnString);
   console.log("Peril game server connected to RabbitMQ!");
+
+  const confirmChannel: ConfirmChannel = await conn.createConfirmChannel();
+
+
+  await publishJSON(confirmChannel,
+    ExchangePerilDirect,
+    PauseKey,
+    {
+      isPaused: true
+    },
+  );
 
   ["SIGINT", "SIGTERM"].forEach((signal) =>
     process.on(signal, async () => {
